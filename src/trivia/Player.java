a0b66bc7f36681c4de5f6e6a563e9cedeaccc8b7
bliaowey <62267175+bliaowey@users.noreply.github.com>
2020-04-22 21:43:38 -0400

    wrote the player class

--- src/trivia/Player.java
+++ src/trivia/Player.java
@@ -1,9 +1,21 @@
 package trivia;
 
 public class Player {
+	String name;
+	int points;
 
 	public Player() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public Player(String name, int points){
+		this.name=name;
+		this.points=points;
+	}
+
+	public String toString(){
+		System.out.println("Player name: "+this.name);
+		System.out.println("Points scored: "+this.points);
 	}
 
 }
