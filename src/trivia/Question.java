363ed4c0ff1d2156b9b06f6097eb6d2f818ffef5
bliaowey <62267175+bliaowey@users.noreply.github.com>
2020-04-23 21:55:34 -0400

    first attempt at the QuestionInput class

--- src/trivia/Question.java
+++ src/trivia/Question.java
@@ -5,27 +5,27 @@ import java.util.ArrayList;
 
 
 public class Question {
-	ArrayList<String> question; 
-	
-	/*this class' constructor creates a ArrayList question 
+	ArrayList<String> question;
+
+	/*this class' constructor creates a ArrayList question
 	 * which takes in an array of Strings as a parameter
-	 * 
+	 *
 	 * for question, question[0] will always be the actual question
 	 * question[1] will always be the right answer
 	 * the following will be wrong answers
-	 */	
+	 */
 	public Question(String[] values) {
 		this.question = new ArrayList<String>();
 		for (int i=0; i<values.length; i++) {
 			question.add(values[i]);
 		}
-		
+
 	}
-	
+
 	public Question(ArrayList<String> question){
 		this.question=question;
 	}
-	
+
 	public String toString(){
 		String toPrint="{";
 		for (int i=0; i<this.question.size(); i++) {
@@ -39,16 +39,16 @@ public class Question {
 		toPrint+="}";
 		return toPrint;
 	}
-	
-	
+
+
 	public int getSize(){
 		return this.question.size();
 	}
-	
+
 	public String getQuesAtLoc(int loc){
 		return question.get(loc);
 	}
-	
+
 	public boolean equals(Question question2){
 		boolean isEqual=true;
 		if(question2.getSize()!=this.question.size()){
@@ -61,5 +61,5 @@ public class Question {
 		}
 		return isEqual;
 	}
-	
+
 }
