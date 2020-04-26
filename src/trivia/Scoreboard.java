0eb3b19d788cd1aae51092c277940a547c0f02a3
Otherfon <liufw2@gmail.com>
2020-04-25 21:03:52 -0400

    Basic code for Scoreboard

--- src/trivia/Scoreboard.java
+++ src/trivia/Scoreboard.java
@@ -1,10 +1,13 @@
 package trivia;
+import java.util.Scanner;
 
 public class Scoreboard {
+	int playerpoints = Player.points
+	int playername = Player.name
 
 	public Scoreboard(Player[] players) {
-		// TODO Auto-generated constructor stub
-		
+		 System.out.println("Player" + /*placeholder for player number variable*/ + "Has" + playername);
+
 	}
 
 }
